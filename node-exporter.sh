54e70ede16efa0f864e0ae63d4d3be2394e07864
mist941 <statkevich.ivan999@gmail.com>
2025-04-09 21:53:52 +0300

    Remove docker-setup.sh script

---
+++ node-exporter.sh
@@ -0,0 +1,7 @@
+cd /opt
+
+sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+
+sudo tar -xzf node_exporter-1.9.1.linux-amd64.tar.gz
+
+sudo mv node_exporter-1.9.1.linux-amd64 node_exporter
