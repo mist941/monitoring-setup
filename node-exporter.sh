d4e2d89267ebf161165a1fdeb266ac5164c9f7b8
mist941 <statkevich.ivan999@gmail.com>
2025-04-10 20:51:32 +0300

    Add systemd service for Node Exporter and move binary to /usr/local/bin

--- node-exporter.sh
+++ node-exporter.sh
@@ -7,3 +7,28 @@ sudo tar -xzf node_exporter-1.9.1.linux-amd64.tar.gz
 sudo mv node_exporter-1.9.1.linux-amd64 node_exporter
 
 sudo rm node_exporter-1.9.1.linux-amd64.tar.gz
+
+sudo mv node_exporter /usr/local/bin/
+
+sudo useradd -rs /bin/false node_exporter
+
+cat <<EOF >/etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+
+sudo systemctl start node_exporter
+
+sudo systemctl enable node_exporter
