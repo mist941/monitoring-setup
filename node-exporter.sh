1a0e833cd883da1137ddee194467832d3acf6ef4
mist941 <statkevich.ivan999@gmail.com>
2025-04-10 20:32:13 +0300

    Remove the downloaded tar.gz file after extracting node_exporter

--- node-exporter.sh
+++ node-exporter.sh
@@ -5,3 +5,5 @@ sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/n
 sudo tar -xzf node_exporter-1.9.1.linux-amd64.tar.gz
 
 sudo mv node_exporter-1.9.1.linux-amd64 node_exporter
+
+sudo rm node_exporter-1.9.1.linux-amd64.tar.gz
