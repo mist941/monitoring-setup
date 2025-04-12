3291ce19121ef31f211fe5c91700381a71e17498
mist941 <statkevich.ivan999@gmail.com>
2025-04-12 12:28:08 +0300

    Enhance Promtail installation script by removing the downloaded zip file and adding configuration for positions and scrape jobs

--- promtail.sh
+++ promtail.sh
@@ -4,6 +4,8 @@ wget https://github.com/grafana/loki/releases/latest/download/promtail-linux-amd
 
 unzip promtail-linux-amd64.zip
 
+sudo rm promtail-linux-amd64.zip
+
 sudo mv promtail-linux-amd64 /usr/local/bin/promtail
 
 sudo useradd -rs /bin/false promtail
@@ -14,6 +16,18 @@ cat <<EOF >/etc/promtail/config.yaml
 server:
   http_listen_port: 9080
   grpc_listen_port: 0
+positions:
+  filename: /tmp/positions.yaml
+clients:
+  - url: http://localhost:3100/api/v1/push
+scrape_configs:
+  - job_name: system
+    static_configs:
+      - targets:
+          - localhost
+      - labels:
+          job: varlogs
+          __path__: /var/log/*.log
 EOF
 
 cat <<EOF >/etc/systemd/system/promtail.service
